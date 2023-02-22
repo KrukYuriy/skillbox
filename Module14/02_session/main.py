075cbf543224b1955660c738bded4e4529643cf5
YURIY <betomi@mail.ru>
2023-02-22 10:37:59 +0300

    Выполнение

--- Module14/02_session/main.py
+++ Module14/02_session/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+print("Введите первую точку")
+x1 = float(input('X: '))
+y1 = float(input('Y: '))
+print("\nВведите вторую точку")
+x2 = float(input('X: '))
+y2 = float(input('Y: '))
+
+x_diff = x1 - x2
+y_diff = y1 - y2
+if x1 == x2:
+    print('Уравнение вида y = k * x + b составить нельзя, т.к. x1 - x2 = 0, а на 0 делить нельзя')
+else:
+    k = y_diff / x_diff
+    b = y2 - k * x2
+
+    print("Уравнение прямой, проходящей через эти точки:")
+    print("y = ", k, " * x + ", b)
--- Module14/03_sum_and_dif/main.py
+++ Module14/03_sum_and_dif/main.py
@@ -1,1 +1,24 @@
-# TODO здесь писать код
+def summ(N):
+    summ = 0
+    while N > 0:
+        num = N % 10
+        summ = summ + num
+        N = N // 10
+    print('Сумма чисел:', summ)
+    return summ
+def amount(N):
+    count = 0
+    while N > 0:
+        count += 1
+        N //= 10
+    print('Количество цифр в числе:', count)
+    return count
+def difference(summ, count):
+    difference = summ - count
+    print('Разность суммы и количества цифр:', difference)
+
+N = int(input('Введите число: '))
+
+summ = summ(N)
+count = amount(N)
+difference(summ, count)
