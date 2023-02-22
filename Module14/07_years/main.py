fd350f691402a5eb141a4a0d6e38ed2f19f2982f
YURIY <betomi@mail.ru>
2023-02-22 18:56:15 +0300

    Выполнено

--- Module14/07_years/main.py
+++ Module14/07_years/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+first_year = int(input('Введите первый год: '))
+second_years = int(input('Введите второй год: '))
+
+print('\nГода от', first_year, 'до', second_years, 'тремя одинаковыми цифрами:')
+
+for i in range(first_year, second_years + 1):
+  a,b,c,d = i // 1000, i // 100 % 10, i // 10 % 10, i % 10
+  if a == b == c or b == c == d or c == d == a or a == b == d:
+    print(a * 1000 + b * 100 + c * 10 + d)
