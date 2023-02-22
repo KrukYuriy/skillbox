9426c5170b1bf163ce776633552e8b4beedca54c
YURIY <betomi@mail.ru>
2023-02-22 12:37:06 +0300

    Выполнено

--- Module14/05_least_divisor/main.py
+++ Module14/05_least_divisor/main.py
@@ -1,10 +1,10 @@
 def min_divisor(num):
-    if num%2 == 0:
+    if num % 2 == 0:
       return 2
     i = 3
-    while num%i != 0 and i*i <= num:
-        i+= 2
-    if i*i <= num:
+    while num % i != 0 and i * i <= num:
+        i += 2
+    if i * i <= num:
         return i
     return num
 
