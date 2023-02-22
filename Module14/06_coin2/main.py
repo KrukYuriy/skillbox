e89fa19ea992dcbad6d3cf8335869cf3af4d1010
YURIY <betomi@mail.ru>
2023-02-22 18:40:39 +0300

    Выполнено

--- Module14/06_coin2/main.py
+++ Module14/06_coin2/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+def check_coordinate(x, y, r):
+    return (x * x + y * y) ** 0.5 < r
+
+print('Введите координаты монетки:')
+coordinate_x = float(input('X: '))
+coordinate_y = float(input('Y: '))
+radius = int(input('Введите радиус: '))
+
+if check_coordinate(coordinate_x, coordinate_y, radius):
+    print('\nМонетка где-то рядом.')
+else:
+    print('\nМонетки в области нет.')
