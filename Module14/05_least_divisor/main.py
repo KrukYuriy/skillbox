119bc737694a5844029c067b96dd14a882c04b05
YURIY <betomi@mail.ru>
2023-02-22 12:31:12 +0300

    Выполнено

--- Module14/05_least_divisor/main.py
+++ Module14/05_least_divisor/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+def min_divisor(num):
+    if num%2 == 0:
+      return 2
+    i = 3
+    while num%i != 0 and i*i <= num:
+        i+= 2
+    if i*i <= num: return i
+    return num
+
+number = int(input('Введите число: '))
+
+print('Наименьший делитель, отличный от единицы:', min_divisor(number))
